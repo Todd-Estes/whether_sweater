c23d8de4b7db75cd86caf00142c4d3cf7f2057b6
todd-estes <elestes@gmail.com>
2021-01-16 21:46:01 -0700

    Create user creation spec

--- spec/requests/api/v1/user_creation_spec.rb
+++ spec/requests/api/v1/user_creation_spec.rb
@@ -8,15 +8,38 @@ describe 'Users API' do
       'ACCEPT' => 'application/json'
     }
 
-    params = {
+    create_params = {
       'email': 'whatever@example.com',
       'password': 'password',
       'password_confirmation': 'password'
     }
 
-    post '/api/v1/users', params, headers: headers
+    params_body = JSON.generate(create_params)
+
+    post '/api/v1/users', headers: headers, params: params_body
 
     expect(response).to be_successful
-
+    var = JSON.parse(response.body, symbolize_names: true)
     end
+
+    it "can fail to create account" do
+
+    headers = {
+      'CONTENT-TYPE' => 'application/json',
+      'ACCEPT' => 'application/json'
+    }
+
+    create_params = {
+      # 'email': 'whatever@example.com',
+      'password': 'password',
+      'password_confirmation': '42069'
+    }
+
+    params_body = JSON.generate(create_params)
+
+    post '/api/v1/users', headers: headers, params: params_body
+
+    expect(response.body).to eq("ERROR: email can't be blank; password_confirmation doesn't match Password")
+    expect(response.status).to eq 400
   end
+end
