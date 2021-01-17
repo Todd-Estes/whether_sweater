fcdf2a444981046f7487510c6774cfce4816bdff
todd-estes <elestes@gmail.com>
2021-01-16 17:27:18 -0700

    Update forecast controller with forecast serializer action

--- app/controllers/api/v1/forecast_controller.rb
+++ app/controllers/api/v1/forecast_controller.rb
@@ -1,7 +1,9 @@
 class Api::V1::ForecastController < ApplicationController
 
   def show
-
+    city = params[:location]
+    forecast = ForecastFacade.get_weather(city)
+    hash = render json: ForecastSerializer.new(forecast)
   end
 
 end
