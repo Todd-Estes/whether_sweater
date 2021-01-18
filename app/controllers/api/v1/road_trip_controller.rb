c04cd29a7030e7ffe3329bdafa8a3870d1f49132
todd-estes <elestes@gmail.com>
2021-01-18 10:57:36 -0700

    Create roadtrip controller

---
+++ app/controllers/api/v1/road_trip_controller.rb
@@ -0,0 +1,19 @@
+class Api::V1::RoadTripController < ApplicationController
+
+  def show
+    reg_user = User.find_by(api_key: road_trip_params[:api_key])
+      if reg_user
+        trip_data = RoadTripFacade.create_trip(road_trip_params[:origin], road_trip_params[:destination])
+        require "pry"; binding.pry
+        render json: RoadtripSerializer.new(trip_data)
+      else
+        render body: "Unauthorized: invalid or missing token", status: 401
+      end
+  end
+
+  private
+
+    def road_trip_params
+      params.permit(:origin, :destination, :api_key)
+    end
+end
