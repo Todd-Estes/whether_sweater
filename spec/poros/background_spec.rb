9630ce890314ccec9bd234ed378fd4511288cf76
todd-estes <elestes@gmail.com>
2021-01-18 10:50:20 -0700

    Final spec creation / refactor

---
+++ spec/poros/background_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+describe 'Background Poro' do
+  it "exists" do
+    location = 'denver,co'
+    image_data = BackgroundService.get_image(location)
+    background_image= BackgroundImage.new(image_data[:value][0], location)
+
+    expect(background_image).to be_a(BackgroundImage)
+    expect(background_image.id).to eq(nil)
+    expect(background_image.image).to be_a(Hash)
+    expect(background_image.image).to have_key(:location)
+    expect(background_image.image[:location]).to be_a(String)
+    expect(background_image.image).to have_key(:image_url)
+    expect(background_image.image[:image_url]).to be_a(String)
+    expect(background_image.image).to have_key(:credit)
+    expect(background_image.image[:credit]).to be_a(Hash)
+    expect(background_image.image[:credit]).to have_key(:source)
+    expect(background_image.image[:credit][:source]).to be_a(String)
+    expect(background_image.image[:credit]).to have_key(:author)
+    expect(background_image.image[:credit][:author]).to be_a(String)
+    expect(background_image.image[:credit]).to have_key(:logo)
+    expect(background_image.image[:credit][:logo]).to be_a(String)
+  end
+end
