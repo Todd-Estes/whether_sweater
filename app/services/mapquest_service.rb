78c6e1d9fd63becdab8726c9190e19eec3d2a8a0
todd-estes <elestes@gmail.com>
2021-01-18 10:57:00 -0700

    Update mapquest trip_results method with new origin and destination params

--- app/services/mapquest_service.rb
+++ app/services/mapquest_service.rb
@@ -6,6 +6,15 @@ class MapquestService
     JSON.parse(response.body, symbolize_names: true)
   end
 
+  def self.trip_results(origin, destination)
+    response = self.conn.get('/directions/v1/route?') do |f|
+      f.params[:from] = origin
+      f.params[:to] = destination
+    end
+    JSON.parse(response.body, symbolize_names: true)
+  end
+
+
   def self.conn
     Faraday.new('http://www.mapquestapi.com') do |f|
       f.params[:key] = ENV['MAPQUEST_API_KEY']
