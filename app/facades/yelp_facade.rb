5f5a84f4323a97372aa0f36addcf92bc4cbdfa06
todd-estes <elestes@gmail.com>
2021-01-19 10:46:07 -0700

    Update yelp facade for time argument

--- app/facades/yelp_facade.rb
+++ app/facades/yelp_facade.rb
@@ -1,5 +1,5 @@
 class YelpFacade
-  def self.get_restaurant(params)
-    restaurant = YelpService.find_place(params)
+  def self.get_restaurant(params, time)
+    restaurant = YelpService.find_place(params, time)
   end
 end
