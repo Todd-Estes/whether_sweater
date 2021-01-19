4c134fbfe02660f14022e7adec595b5a94381050
todd-estes <elestes@gmail.com>
2021-01-18 20:00:00 -0700

    Update params protocol used in road trip controller

--- app/controllers/api/v1/road_trip_controller.rb
+++ app/controllers/api/v1/road_trip_controller.rb
@@ -3,10 +3,10 @@ class Api::V1::RoadTripController < ApplicationController
   def index
     reg_user = User.find_by(api_key: road_trip_params[:api_key])
       if reg_user
-        trip_data = RoadTripFacade.create_trip(road_trip_params[:origin], road_trip_params[:destination])
+        trip_data = RoadTripFacade.create_trip(road_trip_params)
         if trip_data.class == RoadTrip
           render json: RoadtripSerializer.new(trip_data)
-        else
+        elsif trip_data.class == String
           render body: "ERROR: #{trip_data}", status: 400
         end
       else
