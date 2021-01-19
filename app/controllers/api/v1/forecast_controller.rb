55c254bd13c1c63edb8ebadbb6b9461b272066a7
todd-estes <elestes@gmail.com>
2021-01-18 20:00:30 -0700

    Update to forecast controller to test sad path error messaging

--- app/controllers/api/v1/forecast_controller.rb
+++ app/controllers/api/v1/forecast_controller.rb
@@ -3,8 +3,10 @@ class Api::V1::ForecastController < ApplicationController
   def index
     location = params[:location]
     forecast = ForecastFacade.get_weather(location)
-    render json: ForecastSerializer.new(forecast)
-
+      if forecast.class == Forecast
+        render json: ForecastSerializer.new(forecast)
+      else
+        render body: "ERROR: #{forecast[:info][:messages][0]}", status: 400
+      end
   end
-
 end
