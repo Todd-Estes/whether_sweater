419b45f3365d4f6dc457bfd8c41c23037b2d04b2
todd-estes <elestes@gmail.com>
2021-01-18 19:55:41 -0700

    Update road_trip poro instatiation method

--- app/poros/road_trip.rb
+++ app/poros/road_trip.rb
@@ -1,25 +1,21 @@
 class RoadTrip
   attr_reader :id, :start_city, :end_city, :travel_time, :weather_at_eta
 
-  def initialize(trip_data, origin=nil, destination=nil)
-    @id = nil
-    if trip_data.class == Array
-      @start_city = trip_data[0]
-      @end_city = trip_data[1]
-      @travel_time = trip_data[2]
+  def initialize(trip_data, params, weather_array=nil)
+      @id = nil
+    if trip_data[:info][:statuscode] == 0
+      @start_city = params[:origin]
+      @end_city = params[:destination]
+      @travel_time = weather_array.time
       @weather_at_eta = {
-        temperature: trip_data[3],
-        conditions: trip_data[4]
+        temperature: weather_array.temperature,
+        conditions: weather_array.conditions
       }
     else
-      @start_city = origin
-      @end_city = destination
+      @start_city = params[:origin]
+      @end_city = params[:destination]
       @travel_time = "impossible"
       @weather_at_eta = nil
     end
   end
 end
-
-# def convert_time(seconds)
-#   "%02d:%02d:%02d" % [seconds / 3600, seconds / 60 % 60, seconds % 60]
-# end
