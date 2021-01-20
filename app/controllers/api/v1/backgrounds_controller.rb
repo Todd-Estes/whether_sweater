76b7eb9062b283147b9632b5df8a2cf1bd6b0f93
todd-estes <elestes@gmail.com>
2021-01-20 03:32:35 -0700

    Add forecast params private method to forecast controller and sad resp

--- app/controllers/api/v1/backgrounds_controller.rb
+++ app/controllers/api/v1/backgrounds_controller.rb
@@ -1,8 +1,18 @@
 class Api::V1::BackgroundsController < ApplicationController
 
   def index
-    location = params[:location]
-    pic_data = BackgroundsFacade.get_background(location)
-    render json: ImageSerializer.new(pic_data)
+    if background_params[:location].empty?
+      render body: "ERROR: Location entry cannot be blank", status: 400
+    else
+      location = background_params[:location]
+      pic_data = BackgroundsFacade.get_background(location)
+      render json: ImageSerializer.new(pic_data)
+    end
   end
+
+  private
+
+    def background_params
+      params.permit(:location)
+    end
 end
