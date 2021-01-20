482269f3f17b351f13551e28977287da454006bc
todd-estes <elestes@gmail.com>
2021-01-20 03:35:40 -0700

    Add private forecast params method to forecast controller

--- app/controllers/api/v1/forecast_controller.rb
+++ app/controllers/api/v1/forecast_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::ForecastController < ApplicationController
 
   def index
-    location = params[:location]
+    location = forecast_params[:location]
     forecast = ForecastFacade.get_weather(location)
       if forecast.class == Forecast
         render json: ForecastSerializer.new(forecast)
@@ -9,4 +9,11 @@ class Api::V1::ForecastController < ApplicationController
         render body: "ERROR: #{forecast[:info][:messages][0]}", status: 400
       end
   end
+
+
+  private
+
+    def forecast_params
+      params.permit(:location)
+    end
 end
