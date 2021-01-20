262ec2ed638ff5269f1c04d470454bfb4f0da721
todd-estes <elestes@gmail.com>
2021-01-20 03:27:37 -0700

    Update hourly poro with find_direction tests

--- spec/poros/hourly_spec.rb
+++ spec/poros/hourly_spec.rb
@@ -1,6 +1,6 @@
 require "rails_helper"
 
-describe 'Hourly Poro' do
+describe 'Hourly Poro', :vcr do
     before :each do
       location = 'austin,tx'
       map_results = MapquestService.city_results(location)
@@ -21,10 +21,23 @@ describe 'Hourly Poro' do
       expect(@hourly_poro[0].icon).to be_a(String)
     end
 
-    #Cannot find way to test the below!
-  # describe 'instance methods' do
-  #   xit 'finds wind direction based off direction degree' do
-  #     expect(@hourly_poro.find_direction(@data[0][:wind_deg].to_f)).to eq('from NNE')
-  #   end
-  # end
+  describe 'instance methods' do
+    it 'finds wind direction based off direction degree' do
+      expect(@hourly_poro[0].find_direction(15)).to eq('from NNE')
+      expect(@hourly_poro[0].find_direction(40)).to eq('from NE')
+      expect(@hourly_poro[0].find_direction(60)).to eq('from ENE')
+      expect(@hourly_poro[0].find_direction(90)).to eq('from E')
+      expect(@hourly_poro[0].find_direction(110)).to eq('from ESE')
+      expect(@hourly_poro[0].find_direction(130)).to eq('from SE')
+      expect(@hourly_poro[0].find_direction(150)).to eq('from SSE')
+      expect(@hourly_poro[0].find_direction(170)).to eq('from S')
+      expect(@hourly_poro[0].find_direction(200)).to eq('from SSW')
+      expect(@hourly_poro[0].find_direction(220)).to eq('from SW')
+      expect(@hourly_poro[0].find_direction(240)).to eq('from WSW')
+      expect(@hourly_poro[0].find_direction(260)).to eq('from W')
+      expect(@hourly_poro[0].find_direction(290)).to eq('from WNW')
+      expect(@hourly_poro[0].find_direction(310)).to eq('from NW')
+      expect(@hourly_poro[0].find_direction(350)).to eq('from N')
+    end
+  end
 end
